2591fb85491ee527b5548cb9a857922741111d0d
Tracy Speed <tspeed@me.com>
2018-03-09 14:45:03 -0600

    added rows to coffee mug database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  mug1 = Mug.new(
+    color: Faker::Color.color_name,
+    size: Faker::Number.between(12,32),
+    price: Faker::Number.decimal(2)
+    )
+  mug1.save
+end
